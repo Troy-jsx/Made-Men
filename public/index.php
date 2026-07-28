9e7adb71fa9929acb2cb17fd444657c2dcade8fe
Troy Evans <251096@virtualwindow.co.za>
2026-07-28 18:16:08 +0200

    Signup Progress
    Frontend design of signup almost done
    started on input fields
    created a router of sorts

--- public/index.php
+++ public/index.php
@@ -5,8 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Made Men</title>
     <link href="../src/output.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=KoHo:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1 class="text-red-500 text-5xl">Hello</h1>
+    <?php
+        session_start(); // we start a session for our dude
+
+        $page = $_GET['page'] ?? 'signup'; 
+        // gets the dude's current url and if it like not there or smth we set it to signup
+
+        $routes = [
+            // dir is THIS files path and we concat it to the route we want so it takes it to the right place
+            'signup' => __DIR__ . '/../views/auth/signup.php',
+            'login' => __DIR__ .'/../views/auth/login.php',
+        ];
+
+        if (array_key_exists($page, $routes)) { //are they real 
+            include $routes[$page]; //send to the page we need using the array
+        } else {
+            echo "Page not found."; //uh oh we lost
+        }
+    ?>
 </body>
 </html>
---
+++ views/partials/formInput.php
@@ -0,0 +1,6 @@
+<input
+    type="text"
+    name="email"
+    placeholder="eg: mobster@mob.com"
+    class="caret-mmRed w-full px-4 py-2.5 bg-transparent border-b-3 border-inputGrey rounded-sm font-koho font-bold text-4xl focus:outline-none"
+>
