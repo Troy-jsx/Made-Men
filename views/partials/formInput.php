3dcd5e04d1166a2660d619cb4fbe1c62ce76daf7
Troy Evans <251096@virtualwindow.co.za>
2026-07-29 19:17:36 +0200

    Signup form progress

--- views/partials/formInput.php
+++
@@ -1,6 +0,0 @@
-<input
-    type="text"
-    name="email"
-    placeholder="eg: mobster@mob.com"
-    class="caret-mmRed w-full px-4 py-2.5 bg-transparent border-b-3 border-inputGrey rounded-sm font-koho font-bold text-4xl focus:outline-none"
->
---
+++ views/partials/signupForm.php
@@ -0,0 +1,24 @@
+<form class="flex flex-col gap-16 absolute aspect-[600/650] h-[650px] bg-indigo-50">
+    <label class='font-koho font-bold text-8xl text-shadow-lg/40'>Sign Up</label>
+
+    <div class='flex flex-col gap-8'>
+        
+        <div class='flex flex-col gap-1'> 
+            <label class='font-koho font-bold text-[40px] text-shadow-lg/40'>Email Address</label>
+            <input type="email" name="email" placeholder="eg: mobster@mob.com" class="caret-mmRed w-full px-4 py-2.5 bg-transparent border-3 shadow-xl/40 border-inputGrey rounded-sm font-koho font-bold text-4xl focus:outline-none">
+        </div>
+
+        <div class='flex flex-col gap-1'> 
+            <label class='font-koho font-bold text-[40px] text-shadow-lg/40'>Username</label>
+            <input type="text" name="username" placeholder="eg: Joey_Torino" class="caret-mmRed w-full px-4 py-2.5 bg-transparent border-3 shadow-xl/40 border-inputGrey rounded-sm font-koho font-bold text-4xl focus:outline-none">
+        </div>
+
+        <div class='flex flex-col gap-1'> 
+            <label class='font-koho font-bold text-[40px] text-shadow-lg/40'>Password</label>
+            <input type="password" name="password" placeholder="******" class="caret-mmRed w-full px-4 py-2.5 bg-transparent border-3 shadow-xl/40 border-inputGrey rounded-sm font-koho font-bold text-4xl focus:outline-none">
+        </div>
+
+    </div>
+
+
+</form>
