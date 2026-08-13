e3475b9cf2ba8c9609a792ce4935fd68fad0108f
Troy Evans <251096@virtualwindow.co.za>
2026-08-13 18:39:46 +0200

    preGameSelect Screen work

--- views/game/preGameMobSelect.php
+++ views/game/preGameMobSelect.php
@@ -1,3 +1,6 @@
-<div class="bg-[url('../public/img/paperBG.png')] drop-shadow-[0px_0px_10px_rgba(0,0,0,0.65)] flex rotate-87 align-center bg-cover bg-no-repeat absolute left-0 h-[105dvh] aspect-1862/843">
-
-</div>
+<body class="bg-[url('../public/img/WoodBG.png')] bg-cover bg-center bg-no-repeat min-h-screen overflow-hidden">
+    <div class='min-h-screen overflow-hidden relative'>
+        <div class="bg-[url('../public/img/paperBG.png')] left-[-50vw] bg-amber-50 drop-shadow-[0px_0px_10px_rgba(0,0,0,0.65)] flex rotate-87 align-center bg-cover bg-no-repeat absolute h-[100dvh] aspect-1862/843">
+        </div>
+    </div>
+</body>
